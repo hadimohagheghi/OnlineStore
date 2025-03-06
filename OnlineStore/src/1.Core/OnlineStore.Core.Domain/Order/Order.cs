4da66062ae0637ff97029598b4ccba5f17513976
mahshid pahlavan <mahshidPahlavan@gmail.com>
2025-03-06 14:37:20 +0330

    Add Order in Entity

---
+++ OnlineStore/src/1.Core/OnlineStore.Core.Domain/Order/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Domain.Order
+{
+    public class Order
+    {
+
+    }
+}
